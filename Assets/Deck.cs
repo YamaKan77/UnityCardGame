14ffa24dc01837772e916e2a5d88072b568ac361
Yamakan77 <kanyamamoto77@gmail.com>
2017-06-18 12:14:15 -0700

    Fixed shuffle

--- Assets/Deck.cs
+++ Assets/Deck.cs
@@ -6,17 +6,34 @@ public class Deck
 {
 
 	private Card[] deck;
+	private int cardsUsed;
 
 	public Deck()
 	{
 		int cardCount = 0;
 		for (int i = 0; i <= 3; i++)
-			for (int j = 3; j <= 15; j++) {
+			for (int j = 3; j <= 15; j++) 
+			{
 				deck [cardCount] = new Card (j, i);
 				cardCount++;
 			}
-
-
+		cardsUsed = 0;
 	}
 
+	void shuffle(){
+		for (int i = 0; i > deck.Length; i++) 
+		{
+			Card temp = deck[i];
+			int randomIndex = Random.Range (0, deck.Length);
+			deck[i] = deck [randomIndex];
+			deck[randomIndex] = temp;
+		}
+		cardsUsed = 0;
+	}
+
+	public Card dealCard()
+	{
+		cardsUsed++;
+		return deck [cardsUsed - 1];
+	}
 }
---
+++ Assets/Hand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand {
+
+	ArrayList hand = new ArrayList();
+	bool roundDone;
+	bool gameDone;
+	bool start;
+	string ID;
+
+	public Hand()
+	{
+		roundDone = false;
+		gameDone = false;
+		start = false;
+		ID = null;
+	}
+
+	public void addCard(Card c)
+	{
+		hand.Add(c);
+	}
+
+	public void remove(Card c)
+	{
+		for (int i = 0; i < hand.Count; i++) {
+			
+		}
+	}
+}
